0f425799a8dcb2f84c3909cfbae594e5e646c3b6
nhan4599a <nhan0385790927@gmail.com>
2021-06-16 16:29:08 +0700

    Done layout for splash screen activity

---
+++ app/src/main/java/vlu/k24/sep/VLU_Notify_App/Activity/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package vlu.k24.sep.VLU_Notify_App.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import vlu.k24.sep.VLU_Notify_App.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_screen);
+    }
+}
